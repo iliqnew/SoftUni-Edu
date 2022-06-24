28e001302186155fa5173caa11677e33e2654b00
i.iliev <i.t.iliev@armf.bg>
2022-06-24 21:00:43 +0300

    5. Orders

--- Programming Fundamentals/2022/May/Python/6. Basic Syntax, Conditional Statements and Loops - Exercise/5. Orders.py
+++ Programming Fundamentals/2022/May/Python/6. Basic Syntax, Conditional Statements and Loops - Exercise/5. Orders.py
@@ -2,5 +2,19 @@ import random
 
 n = int(input())
 
+total = 0
 for i in range(n):
-    pass
+    price_per_capsule = float(input())
+    days = int(input())
+    capsules_per_day = int(input())
+    if not (0.01 <= price_per_capsule <= 100):
+        continue
+    if not (1 <= days <= 31):
+        continue
+    if not (1 <= capsules_per_day <= 2000):
+        continue
+    total_price = price_per_capsule * days * capsules_per_day
+    print(f"The price for the coffee is: ${total_price:.2f}")
+    total += total_price
+
+print(f"Total: ${total:.2f}")
